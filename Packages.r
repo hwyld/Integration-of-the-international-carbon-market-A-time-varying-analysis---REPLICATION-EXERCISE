663bda0ea0c93786439ffa409e59d9bc110b2cd1
hwyld <henry_wyld@hotmail.com>
2024-03-21 11:17:26 +1000

    install packages file

---
+++ Packages.r
@@ -0,0 +1,37 @@
+# ECOM90022 Research Methods Semester 1, 2024
+# Helpful functions for installing and loading packages
+# Henry Wyld
+
+# Function to install and load packages
+load_packages <- function() {
+  # List of packages with descriptions
+  packages <- list(
+    "ggplot2" = "flexible package for plotting in R", 
+    "rio" = "easily read data into R", 
+    "car" = "joint linear restrictions in R", 
+    "lmtest" = "hypothesis tests in R", 
+    "sandwich" = "robust variance estimator in R", 
+    "quantmod" = "download financial data", 
+    "dynlm" = "time-series models in R", 
+    "estimatr" = "robust standard errors in R", 
+    "zoo" = "handling time-series dates in R", 
+    "broom" = "used for ggplot2 tidy", 
+    "tidyr" = "used for ggplot2 spread", 
+    "scales" = "used for formatting scales in ggplot2",
+    "dplyr" = "data manipulation and transformation", 
+    "stringr" = "string operations", 
+    "lubridate" = "date-time manipulations"
+  )
+
+  # Function to check and install packages
+  check_and_install <- function(pkg, desc){
+    if(!pkg %in% installed.packages()){
+      install.packages(pkg)
+    }
+    library(pkg, character.only = TRUE)
+    message(paste(pkg, "-", desc))
+  }
+
+  # Apply the function to each package
+  mapply(check_and_install, names(packages), packages)
+}
